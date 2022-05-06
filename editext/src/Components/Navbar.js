c394de0b7f7a08bc000b7fe0467e25364fd982ca
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-05-05 22:10:54 -0400

    Added required files to the APP

--- editext/src/Components/Navbar.js
+++ editext/src/Components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar(props){
 	<div className="container-fluid">
 		{/* Resolve the props.title from the title attribute value in the app.js file */}
 	  {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
-	  <a className="navbar-brand" href="/">{props.title}</a>
+	  <a className="navbar-brand" href="#">{props.title}</a>
 	  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span className="navbar-toggler-icon"></span>
 	  </button>
@@ -25,7 +25,7 @@ export default function Navbar(props){
 	    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 	      <li className="nav-item">
 		{/* <Link className="nav-link active" aria-current="page" to="/">{props.home}</Link> */}
-		<a className="nav-link active" aria-current="page" href="/">{props.home}</a>
+		<a className="nav-link active" aria-current="page" href="#">{props.home}</a>
 	      </li>
 	      <li className="nav-item">
 		{/* <a className="nav-link" href="/about">{props.about}</a> */}
